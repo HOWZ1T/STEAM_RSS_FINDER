57fd74fa8639e112f49e246bac58595164d7b57a
HOWZ1T <dylan.d.randall@gmail.com>
2018-03-18 13:01:13 +1100

    Added two features that make this script more robust.
    Added agecheck detection and bypass.
    Added an fallback method for getting rss feed if main method fails.

--- find_rss.php
+++ find_rss.php
@@ -34,7 +34,7 @@
 		{
 			echo $link."\n";
 		}
-		echo "Most relevant:\n".$parsed_links[0]."\n\n";
+		echo "most relevant:\n".$parsed_links[0]."\n\n";
 
 		if($returnAll == true)
 		{
@@ -75,7 +75,7 @@
 		{
 			echo $link."\n";
 		}
-		echo "Most relevant:\n".$parsed_links[0]."\n\n";
+		echo "most relevant:\n".$parsed_links[0]."\n\n";
 
 		return $parsed_links[0];
 	}
@@ -91,7 +91,68 @@
 		echo "requesting response from: ".$item_link."\n";
 		$res = file_get_contents($item_link);
 		
-		preg_match_all('/.*<a href=\"http:\/\/steamcommunity\.com\/games\/.*\/rss\/.*/', $res, $out);
+		preg_match_all('/.*<a.*href=\"http:\/\/steamcommunity\.com\/games\/.*\/rss\/.*/', $res, $out);
+		$parsed_links = array();
+		foreach($out[0] as $key => $raw_link)
+		{
+			$link = explode('"', $raw_link)[1];
+			array_push($parsed_links, $link);
+		}
+
+		if(count($parsed_links) <= 0)
+		{
+			echo "no links found\n\n";
+			return null;
+		}
+
+		echo "gathered links:\n";
+		foreach($parsed_links as $key => $link)
+		{
+			echo $link."\n";
+		}
+		echo "most relevant:\n".$parsed_links[0]."\n\n";
+
+		return $parsed_links[0];
+	}
+
+	function bypassAgeGate($item_link)
+	{
+		if($item_link == null || strlen($item_link) <= 0)
+		{
+			echo "missing the item_link parameter!";
+			return null;
+		}
+		
+		echo "checking for agegate...\n";
+		echo "requesting response from: ".$item_link."\n";
+		$res = file_get_contents($item_link);
+		preg_match_all('/.*agegate.*/', $res, $out);
+		if(!($out != null && count($out) > 0))
+		{
+			echo "\n\nagegate not found!\n\n";
+			return null;
+		}
+		echo "agegate found\n";
+		echo "attempting to bypass...\n";
+		echo "setting cookie...\n";
+		$curl = curl_init($item_link);
+		curl_setopt($curl, CURLOPT_VERBOSE, 1);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_COOKIE, 'mature_content=1');
+		echo "requesting response via curl from: ".$item_link."\n";
+		$page = curl_exec($curl);
+		curl_close($curl);
+		
+		if($page == false)
+		{
+			echo "\n\ncurl request failed!\n\n";
+			return null;
+		}
+		
+		echo "\n"; //clear curl verbose output
+		//get link
+		$out = null;
+		preg_match_all('/.*<a href=\"http:\/\/steamcommunity\.com\/gid\/.*/', $page, $out);
 
 		$parsed_links = array();
 		foreach($out[0] as $key => $raw_link)
@@ -111,9 +172,146 @@
 		{
 			echo $link."\n";
 		}
-		echo "Most relevant:\n".$parsed_links[0]."\n\n";
+		echo "most relevant:\n".$parsed_links[0]."\n\n";
 
 		return $parsed_links[0];
+	}
+
+	function getRSSviaCommunity($item_link)
+	{
+		echo "attempting to get rss feed via community hub...\n";
+		if($item_link == null || strlen($item_link) <= 0)
+		{
+			echo "missing the item_link parameter!";
+			return null;
+		}
+		
+		echo "checking for agegate...\n";
+		echo "requesting response from: ".$item_link."\n";
+		$res = file_get_contents($item_link);
+		preg_match_all('/.*agegate.*/', $res, $out);
+		if(!($out != null && count($out) > 0))
+		{
+			echo "\n\nagegate not found!\n\n";
+			return null;
+		}
+		echo "agegate found\n";
+		echo "attempting to bypass...\n";
+		echo "setting cookie...\n";
+		$curl = curl_init($item_link);
+		curl_setopt($curl, CURLOPT_VERBOSE, 1);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_COOKIE, 'mature_content=1');
+		echo "requesting response via curl from: ".$item_link."\n";
+		$page = curl_exec($curl);
+		curl_close($curl);
+		
+		if($page == false)
+		{
+			echo "\n\ncurl request failed!\n\n";
+			return null;
+		}
+		
+		echo "\nattempting to retrieve community link...\n"; //clear curl verbose output
+		//get community link
+		$out = null;
+		preg_match_all('/.*<a.*href=\"http:\/\/steamcommunity\.com\/app\/.*\">/', $page, $out);
+		$parsed_links = array();
+		foreach($out[0] as $key => $raw_link)
+		{
+			$link = explode('"', $raw_link)[3];
+			array_push($parsed_links, $link);
+		}
+
+		if(count($parsed_links) <= 0)
+		{
+			echo "no links found\n\n";
+			return null;
+		}
+
+		$community_link = $parsed_links[0];
+		
+		if($community_link == false || $community_link == null)
+		{
+			echo "\n\ncould not find community link!\n\n";
+			return null;
+		}
+		
+		$out = null;
+		echo "found community link: ".$community_link."\n";
+		echo "requesting response from: ".$community_link."\n";
+		$res = file_get_contents($community_link);
+		echo "\nattempting to retrieve news iframe page link...\n"; //clear curl verbose output
+		//get news page link
+		preg_match_all('/.*<a.*href=\"http:\/\/steamcommunity\.com\/app\/.*\/all.*/', $res, $out);
+		$parsed_links = array();
+		foreach($out[0] as $key => $raw_link)
+		{
+			$link = explode('"', $raw_link)[1];
+			array_push($parsed_links, $link);
+		}
+
+		if(count($parsed_links) <= 0)
+		{
+			echo "no links found\n\n";
+			return null;
+		}
+		
+		$out = null;
+		$news_link = $parsed_links[0];
+		echo "found news iframe page link: ".$news_link."\n";
+		echo "attempting to retrieve rss page link...\n";
+		
+		$res = file_get_contents($news_link);
+		//check for age gate
+		$hasAgeGate = false;
+		echo "checking for agegate...\n";
+		echo "requesting response from: ".$news_link."\n";
+		$res = file_get_contents($news_link);
+		preg_match_all('/.*agegate.*/', $res, $out);		
+		$page = $res;
+		if(!($out != null && count($out) > 0))
+		{
+			echo "agegate not found!\n";
+		}
+		else
+		{
+			echo "agegate found\n";
+			echo "attempting to bypass...\n";
+			echo "setting cookie...\n";
+			$curl = curl_init($news_link);
+			curl_setopt($curl, CURLOPT_VERBOSE, 1);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($curl, CURLOPT_COOKIE, 'mature_content=1');
+			echo "requesting response via curl from: ".$news_link."\n";
+			$page = curl_exec($curl);
+			curl_close($curl);
+
+			if($page == false)
+			{
+				echo "\n\ncurl request failed!\n\n";
+				return null;
+			}	
+		}
+		
+		preg_match_all('/.*data-modal-content-url=\"http:\/\/steamcommunity.com\/gid\/.*\"/', $page, $out);
+		$parsed_links = array();
+		foreach($out[0] as $key => $raw_link)
+		{
+			$link = explode('"', $raw_link)[5];
+			array_push($parsed_links, $link);
+		}
+
+		if(count($parsed_links) <= 0)
+		{
+			echo "no links found\n\n";
+			return null;
+		}
+		
+		$out = null;
+		$rss_link = $parsed_links[0];
+		echo "found rss link: ".$rss_link."\n";
+		return $rss_link;
 	}
 
 	$term = "";
@@ -124,8 +322,16 @@
 	$term = strtolower(substr($term, 0, strlen($term)-3));
 	echo "search term = ".$term."\n";
 	
-	$result = getRSS(getUpdateLink(search($term)));
+	$page_link = search($term);
+	if($page_link == null)
+	{
+		die("\n\n\n*********************\n* NO LINKS FOUND :( *\n*********************\n\n\n");
+	}
 
+	$result = getUpdateLink($page_link);
+	if($result == null) {$result = bypassAgeGate($page_link);}
+	if($result == null) {$result = getRSSviaCommunity($page_link);}
+	$result = getRSS($result);
 
 	if($result == null)
 	{
